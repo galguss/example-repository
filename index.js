bf707793a3a5018915719a799eb8afe69972cbc0
galguss <gal.gus12@gmail.com>
2023-01-01 13:36:58 +0200

    two scene

--- index.js
+++ index.js
@@ -3,23 +3,14 @@ async function memePopular()
     const URL = "https://api.imgflip.com/get_memes";
     const res = await fetch(URL);
     const data = await res.json();
-    let populer = data.data.memes[0];
-
-    for(let k = 1; k < data.data.memes.length; k++){
-        if(populer.box_count < data.data.memes[k].box_count)
-        {
-            populer = data.data.memes[k];
-        }
-    }
-
-    console.log(populer);
     const Box = document.getElementById('Box');
 
     const img = document.createElement('img');
-    img.src = populer.url;
+    img.src = data.data.memes[0].url;
+    img.width = "300";
     Box.appendChild(img);
     const name = document.createElement('p');
-    name.innerHTML = populer.name;
+    name.innerHTML = data.data.memes[0].name;
     Box.appendChild(name);
     
 }
