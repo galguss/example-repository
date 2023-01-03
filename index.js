c335a40cc56c6da2f5befc499e75b672607bd726
galguss <gal.gus12@gmail.com>
2023-01-03 10:04:14 +0200

    end Working

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+window.addEventListener("load", ()=> {
+    let container = document.getElementById('loading-container');
+    container.classList = "end-loading";
+})
+
+let indexValue = 0;
+
 async function memePopular()
 {
     const URL = "https://api.imgflip.com/get_memes";
@@ -5,7 +12,7 @@ async function memePopular()
     const data = await res.json();
     const Box = document.getElementById('Box');
 
-    for(let k = 0; k < 3; k++){
+    for(let k = 0; k < 10; k++){
         const img = document.createElement('img');
         img.src = data.data.memes[k].url;
         img.width = "300";
@@ -13,7 +20,25 @@ async function memePopular()
         const name = document.createElement('p');
         name.innerHTML = data.data.memes[k].name;
         Box.appendChild(name);
-    }   
+    }
+    
+    showImg(indexValue);
 }
 
 memePopular();
+
+function side_slide(n){showImg(indexValue += n);}
+function showImg(n){
+    const img = document.querySelectorAll('img');
+    const p = document.querySelectorAll('p');
+    if(n > img.length -1 ){indexValue = 0};
+    if(n < 0){indexValue = img.length -1};
+    for(let k=0; k < img.length; k++){
+        img[k].style.display = "none";
+        p[k].style.display = "none";
+    }
+    img[indexValue].style.display = "block";
+    p[indexValue].style.display = "block";
+}
+
+
